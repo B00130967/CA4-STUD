c6be2c423db16547a9bf3a45d0474172f3d9d267
B0013067 <B00130967@mytudublin.ie>
2020-04-07 18:03:46 +0200

    Initial Setup for CA4 Project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <head> 
+        <meta charset="UTF-8"> 
+        <title>Shopping Cart</title> 
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'> 
+        <!-- Bootstrap 3.3.7 --> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"> 
+    </head> 
+    <body > 
+        <div id="page-content-wrapper"> 
+            <div class="container-fluid"> 
+                <div class="row"> 
+                    <div class="col-lg-2"></div> 
+                    <div class="col-lg-8"> @yield('content') </div> 
+                    <div class="col-lg-2"></div> 
+                </div> 
+            </div> 
+         </div> 
+    </body> 
+</html>
